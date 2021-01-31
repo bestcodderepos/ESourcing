7775288c4d0bac62f3b5d9e4f6e93a85334775c9
hasandenli <hasan_denli@hotmail.com>
2021-01-31 15:52:31 +0300

    Sourcing Service is added

---
+++ Esourcing.Sourcing/Controllers/AuctionController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Esourcing.Sourcing.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class AuctionController : ControllerBase
+    {
+       
+    }
+}
